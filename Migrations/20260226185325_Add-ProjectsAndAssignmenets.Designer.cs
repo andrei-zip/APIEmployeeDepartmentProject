a5e26dd6225fd6ee372f8d0171816a8f0e7fd3c8
amoraru <amoraru@uclan.ac.uk>
2026-02-26 20:55:48 +0200

    Migration build for Project and EmployeeProjects table database

--- Migrations/20260226185325_Add-ProjectsAndAssignmenets.Designer.cs
+++ Migrations/20260226185325_Add-ProjectsAndAssignmenets.Designer.cs
@@ -3,14 +3,16 @@ using APIEmployeeDepartmentProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace APIEmployeeDepartmentProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260226185325_Add-ProjectsAndAssignmenets")]
+    partial class AddProjectsAndAssignmenets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260226185325_Add-ProjectsAndAssignmenets.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIEmployeeDepartmentProject.Migrations
+{
+    public partial class AddProjectsAndAssignmenets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Budget = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    ProjectCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeesProjects",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeesProjects", x => new { x.EmployeeId, x.ProjectId });
+                    table.ForeignKey(
+                        name: "FK_EmployeesProjects_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeesProjects_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeesProjects_ProjectId",
+                table: "EmployeesProjects",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProjectCode",
+                table: "Projects",
+                column: "ProjectCode",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeesProjects");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
