c3561783dc7c1222e65eee82815fa22d54ff20f9
amoraru <amoraru@uclan.ac.uk>
2026-02-26 20:48:40 +0200

    Added DbSets for Projects and EmployeeProjects + Rules for project and assignemnts on projects

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -16,18 +16,42 @@ namespace APIEmployeeDepartmentProject.Data
 
         public DbSet<Employee> Employees => Set<Employee>();
 
+        public DbSet<Project> Projects => Set<Project>();
+
+        //join table for employees and projects
+        public DbSet<EmployeeProject> EmployeesProjects => Set<EmployeeProject>();
 
         // Configs for relationships keys , indexes and entites
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Employee Rules //
             // Make email unique between employees
             modelBuilder.Entity<Employee>().HasIndex(e => e.EmailAddress).IsUnique();
 
             // Configure decimal precision to avoid truncation - no alloweing to lose any cents
             modelBuilder.Entity<Employee>().Property(e => e.Salary).HasPrecision(18, 2);
 
-        }
+            // Project rules //
+            // Project code must be unique
+            modelBuilder.Entity<Project>().HasIndex(p=>p.ProjectCode).IsUnique();
 
-            
+            //Budget precision to avoid truncation
+            modelBuilder.Entity<Project>().Property(p=>p.Budget).HasPrecision(18, 2);
+
+            // EmployeeProject rules //
+            // Makes sure an employee gets assigned only once per project
+            modelBuilder.Entity<EmployeeProject>().HasKey(ep => new { ep.EmployeeId, ep.ProjectId });
+
+            // Relationship - Many Projects per Employee
+            modelBuilder.Entity<EmployeeProject>().HasOne(ep => ep.Employee)
+                .WithMany(e => e.EmployeeProjects)
+                .HasForeignKey(ep => ep.EmployeeId);
+
+            // Relationship - Many Employees per Project
+            modelBuilder.Entity<EmployeeProject>().HasOne(ep => ep.Project)
+                .WithMany(p => p.EmployeeProjects)
+                .HasForeignKey(ep => ep.ProjectId);
+
+        }
     }
 }
