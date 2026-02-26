913662fb4ad9db8ec3c814d6f33b0d783dda5aa5
amoraru <amoraru@uclan.ac.uk>
2026-02-26 20:25:26 +0200

    Created Project.cs and EmployeePorject.cs - join table and relationships

---
+++ Models/EmployeeProject.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIEmployeeDepartmentProject.Models
+{
+    // A class representing a join table between Employees and Projects ( many to many)
+    // Representing the the one to one relationship metween one employee and one project with a role 
+    public class EmployeeProject
+    {
+
+        // The foreign key of Employee
+        public int EmployeeId { get; set; }
+
+        // Allows acces to EmployeePorject - Employee
+        public Employee Employee { get; set; }
+
+        //The foreign key of Project
+        public int ProjectId { get; set; }
+
+        // Allows access to EmployeeProject - Project
+        public Project Project { get; set; }
+
+        // Role of the employee in the project 
+        [Required, MaxLength(100)]
+        public string Role { get; set; }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIEmployeeDepartmentProject.Models
+{
+    // A class representing a Project in a Company
+    // This will be mapped to the SQL table "Porjects"
+    public class Project
+    {
+        // EF Core will automailically treat the "id" primary key in database
+        public int id { get; set; }
+
+        // Project name required
+        [Required,MaxLength(120)]
+        public string name { get; set; }
+
+        // Budget which cannot be negative
+
+        [Range(0,double.MaxValue)]
+        public decimal Budget { get; set; }
+
+        // Unique code for the project generated with the use of external api "Codito"
+
+        [Required,MaxLength(50)]
+        public string ProjectCode { get; set; }
+
+        // Project can have many employees assigned 
+        public ICollection<EmployeeProject> EmployeeProjects { get; set; } = new List<EmployeeProject>();
+       
+        
+
+    }
+}
