4d8e1f91bb5b9625c4dbffb800cf1f8f949c4c27
amoraru <amoraru@uclan.ac.uk>
2026-02-26 14:52:51 +0200

    Created Models file, and Department class

---
+++ Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIEmployeeDepartmentProject.Models
+{
+    // A class reporesenting a Department in a company
+    // That will be mapped to the SQL table "Departments"
+    public class Department
+    {
+        // EF Core will automailically treat the "id" primary key in database
+        public int id { get; set; }
+
+        // API Validation + DB does not allow nulls
+        // Sets limits to the size of the string (name)
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(100)]
+        public string OfficeLocation { get; set; }
+    }
+}
